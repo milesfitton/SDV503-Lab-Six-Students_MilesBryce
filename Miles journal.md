4ccd6437c66a39870d2e1701802b56cdbe1885fa
Miles <Miles-fitton@live.nmit.ac.nz>
2021-04-13 22:36:18 +1200

    Attemt task one in taskone.js

--- Miles journal.md
+++ Miles journal.md
@@ -1,3 +1,4 @@
 //Pair coding exercise
 // learnt about "touch " to create a working document in the branch 
 //eg touch taskone.js
+// git pull is all that is needed to puull from git hup (no filename required)
--- taskone.js
+++ taskone.js
@@ -4,4 +4,24 @@
 Ex:  addOddNumbers(5)  -> 9  // 1 + 3 + 5 = 9
 Ex: addOddnumbers(13) -> 49 // 1 + 3 + 5 + 7 + 9 + 11 + 13 = 49*/
 
-var x = 9 // declaring a variable x with the value of 9
+
+// formula = add 1 to ending number eg 5 +1
+// formula = divide above number by 2 
+//formula = square the above number 
+//the formula to determine the sum between 1 and n is 1/2(n + 1))2
+
+
+function addOddNumbers(n)  {
+    return arr=(n+1)/2
+Math.pow (arr,2)
+}
+console.log(addOddNumbers([5]))
+
+// arr =[2,6] - find out if odd or even number
+ /*function oddOrEven (x) {
+let sum = 0;
+x.forEach (num => sum += num)
+return sum%2 === 0 ? 'even': 'odd'
+};
+console.log(oddOrEven ([1,2,3,4,5]))*/
+
