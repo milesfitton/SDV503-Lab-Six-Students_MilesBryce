5764a8f2f97a49caee1a182ab3480b092d159418
Miles <Miles-fitton@live.nmit.ac.nz>
2021-04-15 22:37:07 +1200

    Attempt at Task 2

---
+++ tasktwo.js
@@ -0,0 +1,16 @@
+/*Task_2: Create a function that takes an array of non-negative integers and strings 
+and return a new array without the strings.
+Ex:  filterArr([1, 2, "a", "b"]) -> [1, 2]
+Ex:  filterArr([1, 2, "aasf", "1", "123", 123]) ➞ [1, 2, 123]
+Note: The given array only has integers and strings.
+Note: Numbers in the array should not repeat.
+Note: The original order must be maintained.*/
+
+//let filterArr =(1, 2, "a", "b") => [a,b,c,d]
+//console.log(typeof filterArr)
+
+function filterArr (arr){
+    return arr.typeof ('')
+     }
+     console.log (filterArr([1,2,"a", "b"))
+     // Hi Bryce, Not sure if you can expand on this - it it not right...
