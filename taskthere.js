0f62733475fe21bdcb39d3a19996d97744403605
Miles <Miles-fitton@live.nmit.ac.nz>
2021-04-15 20:00:31 +1200

    Hi Bryce Check out this code for task three

--- taskthere.js
+++ taskthere.js
@@ -13,52 +13,54 @@ by counts.property_1 and counts.property_2*/
 
 
  //below is the data of likes and dislikes
-let counts= [
+/*let counts= [
     { data: "1", status: "like"},
     { data: "2", status: "like"},
     { data: "3", status: "dislike"},
     { data: "4", status: "dislike"},
     { data: "5", status: "dislike"},
     { data: "6", status: "dislike"}
-  ];
+  ];*/
  
   //below counts the likes
- let counterLikes = 0;
- for (let i = 0; i < counts.length; i++){
-if (counts[i].status === "like") counterLikes++;
- }
- console.log (counterLikes)
+//  let counterLikes = 0;
+//  for (let i = 0; i < counts.length; i++){
+// if (counts[i].status === "like") counterLikes++;
+//  }
+//  console.log (counterLikes)
  
 
  //below counts the dislikes
- let counterDisLikes = 0;
- for (let i = 0; i < counts.length; i++) {
-   if (counts[i].status === "dislike") counterDisLikes++;
- }
- console.log(counterDisLikes)
+//  let counterDisLikes = 0;
+//  for (let i = 0; i < counts.length; i++) {
+//    if (counts[i].status === "dislike") counterDisLikes++;
+//  }
+//  console.log(counterDisLikes)
 
  //below counts the likes (different method - using for of)
 
-let counterLikesAlternative = 0;
-for (let obj of counts){
-  if (obj.status === "like") counterLikesAlternative ++;
-}
-console.log(counterLikesAlternative)
+// let counterLikesAlternative = 0;
+// for (let obj of counts){
+//   if (obj.status === "like") counterLikesAlternative ++;
+// }
+// console.log(counterLikesAlternative)
 
 //below counts the dislikes (using reduce )
-let counterDisLikesReduce = counts.reduce((counterDisLikesReduce,obj) =>{
-  if (obj.status === "dislike") counterDisLikesReduce += 1
-  return counterDisLikesReduce;
-},0);
-console.log (counterDisLikesReduce)
+// let counterDisLikesReduce = counts.reduce((counterDisLikesReduce,obj) =>{
+//   if (obj.status === "dislike") counterDisLikesReduce += 1
+//   return counterDisLikesReduce;
+// },0);
+// console.log (counterDisLikesReduce)
 
-//Below is the ES6 version of the above code
-let counterEs6Reduce = counts.reduce((counterEs6Reduce,obj) =>
-obj.status === "dislike" ? counterEs6Reduce += 1 : counterEs6Reduce , 0)
-console.log (counterEs6Reduce)
-
-
+//Below is the ES6 version that counts the dislikes (using reduce )
+// let counterEs6Reduce = counts.reduce((counterEs6Reduce,obj) =>
+// obj.status === "dislike" ? counterEs6Reduce += 1 : counterEs6Reduce , 0)
+// console.log (counterEs6Reduce)
 
 
-
+function statusObj(obj) {
+  return obj.likes - obj.dislikes
+}
+console.log(statusObj({likes: 4, dislikes: 10}))
+ 
 
